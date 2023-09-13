a5ea6b858f6072b1e796f2137733ad2a5e9b05da
Doğukan Kol <43730866+Dogukankol@users.noreply.github.com>
2023-09-13 19:15:03 +0300

    videos can be made change, category hover effect

--- components/Video/index.jsx
+++ components/Video/index.jsx
@@ -1,54 +1,62 @@
 "use client"
-import React, {useRef, useState} from 'react'
-import Link from 'next/link';
+import React, { useRef, useEffect, useState, useContext } from 'react'
 import Image from 'next/image';
+import {VideoContext} from '@/containers/Home/TopVideos'
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlay } from "@fortawesome/free-solid-svg-icons";
 import { faYoutube } from "@fortawesome/free-brands-svg-icons";
 
-export function VideoThumbnail({ head = "He Speaks 11 Languages – 7 Tricks To Learn Any", time = "20 MIN AGO", width = "200", height = "100" }) {
+export function VideoThumbnail({ videoSrc, author, head = "He Speaks 11 Languages – 7 Tricks To Learn Any", time = "20 MIN AGO", width = "200", height = "100" }) {
+    const { activeVideo, setVideo } = useContext(VideoContext);
+    const handleClick = () => {
+        setVideo({head, time, author, videoSrc});
+        
+    };
     return (
-        <div className="video__thumbnail">
-            <Link href={head}>
-                <figure>
-                    <Image src={`https://dummyimage.com/${width}x${height}/3a3a3a/fff`} width={width} height={height}  alt={head} />
-                    <span><FontAwesomeIcon icon={faPlay} /></span>
-                </figure>
-                <div className="video__content">
-                    <p className='video__head'>{head}</p>
-                    <p className='video__min'>{time}</p>
-                </div>
-            </Link>
-
+        <div className="video__thumbnail" onClick={handleClick}>
+            <figure>
+                <Image src={`https://dummyimage.com/${width}x${height}/3a3a3a/fff`} width={width} height={height} alt={head} />
+                <span><FontAwesomeIcon icon={faPlay} /></span>
+            </figure>
+            <div className="video__content">
+                <p className='video__head'>{head}</p>
+                <p className='video__min'>{time}</p>
+            </div>
         </div>
     )
 }
 
 
-export function VideoFull({ head = "Under Marketing Chief Bruno Cardinali, Popeyes Is Eating", time = "20 MIN AGO", author = "ROKISTAN WAINS" }) {
+export function VideoFull({ videoSrc="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4", head = "Under Marketing Chief Bruno Cardinali, Popeyes Is Eating", time = "20 MIN AGO", author = "ROKISTAN WAINS" }) {
     const videoRef = useRef(null);
-    const [isPlay, setIsPlay] = useState(false)
+    const [isPlay, setIsPlay] = useState(false);
+
+    useEffect(() => {
+        videoRef.current?.load()
+    }, [videoSrc])
+    
+
     const handlePlayVideo = () => {
         videoRef.current.play();
         setIsPlay(true)
     }
     return (
         <div className="video__full">
-                <video controls={isPlay} ref={videoRef} poster='https://dummyimage.com/870x542/3a3a3a/fff'>
-                    <source src="https://www.w3schools.com/tags/movie.mp4" type="video/mp4" />
-                    Your browser does not support the video tag.
-                </video>
-                {
-                    !isPlay && <button className='video__starter' onClick={handlePlayVideo}><FontAwesomeIcon icon={faPlay} /></button>
-                }
-                {
-                    !isPlay && 
-                    <div className="video__content">
-                        <p className='video__head'>{head}</p>
-                        <p className='video__specs'><FontAwesomeIcon icon={faYoutube} /> {author} / {time}</p>
-                    </div>
-                }
+            <video controls={isPlay} ref={videoRef} poster='https://dummyimage.com/870x542/3a3a3a/fff'>
+                <source src={videoSrc} type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
+            {
+                !isPlay && <button className='video__starter' onClick={handlePlayVideo}><FontAwesomeIcon icon={faPlay} /></button>
+            }
+            {
+                !isPlay &&
+                <div className="video__content">
+                    <p className='video__head'>{head}</p>
+                    <p className='video__specs'><FontAwesomeIcon icon={faYoutube} /> {author} / {time}</p>
+                </div>
+            }
 
         </div>
     )
--- containers/Home/TopVideos/index.jsx
+++ containers/Home/TopVideos/index.jsx
@@ -1,34 +1,64 @@
+"use client"
 import { SectionHead, VideoFull, VideoThumbnail } from '@/components'
-import React from 'react'
+import React, { createContext, useContext, useState } from 'react'
+
+export const VideoContext = createContext();
 
 function TopVideos() {
+    const [activeVideo, setActiveVideo] = useState({
+        head: "Under Marketing Chief Bruno Cardinali, Popeyes Is Eating", time: "20 MIN AGO"
+    });
+
+    const setVideo = video => {
+        setActiveVideo(video);
+        console.log(activeVideo)
+    };
+
     const videoList = [{
-        head: "He Speaks 11 Languages - 7 Tricks To Learn Any"
+        head: "He Speaks 11 Languages - 7 Tricks To Learn Any",
+        time: "20 MIN AGO",
+        author: "ROKISTAN WAINS 1",
+        videoSrc: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+        
     },
     {
-        head:"Goodbye To Snoring, SayHello to Sleep"
+        head: "Goodbye To Snoring, SayHello to Sleep",
+        time: "30 MIN AGO",
+        author: "ROKISTAN WAINS 2",
+        videoSrc: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"
+
     },
     {
-        head: "Discovering Sydney’s Must - See Attractions"
+        head: "Discovering Sydney’s Must - See Attractions",        
+        time: "10 MIN AGO",
+        author: "ROKISTAN WAINS 3",
+        videoSrc: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4"
+
     }
-]
+    ]
+
+
+
     return (
         <section className='top-videos'>
             <div className="container">
                 <SectionHead tag="h2" text="Top Videos" />
-                <div className="top-videos__list">
-                    <div className="top-videos__full">
-                        <VideoFull />
-                    </div>
-                    <div className="top-videos__thumbnails">
-                        {
-                            videoList.map((item, key) => (
+                <VideoContext.Provider  value={{ activeVideo, setVideo }}>
+                    <div className="top-videos__list">
+                        <div className="top-videos__full">
+                            {activeVideo && <VideoFull {...activeVideo} /> }
+                        </div>
+                        <div className="top-videos__thumbnails">
+                            {
+                                videoList.map((item, key) => (
 
-                                <VideoThumbnail {...item} key={key} />
-                            ))
-                        }
+                                    <VideoThumbnail {...item} key={key} VideoContext  />
+                                ))
+                            }
+                        </div>
                     </div>
-                </div></div>
+                </VideoContext.Provider>
+            </div>
         </section>
     )
 }
